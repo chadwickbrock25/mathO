f8e2d71316c3a93011706f76fabf342b03c1b1a2
Chadwick Mitchell Brock <Chadwick@Chadwicks-Pro.attlocal.net>
2020-08-07 00:29:22 -0400

    sending text from both servers

--- react_matho/src/App.js
+++ react_matho/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component} from 'react';
 import './App.css';
+const baseURL = "http://localhost:3003";
 
-function App() {
+
+export default class App extends Component {
+  state = {
+    question: '',
+    answer: false,
+    unanswered:[],
+    reviewQ: [],
+    correct: [],
+    score: 0,
+    signUp:false,
+    login:false,
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    loginEmail: "",
+    loginPassword: "",
+    token:"",
+    userid:"",
+    baseURL: baseURL,
+    
+  }
+  play() {
+
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>First steps</h1>
     </div>
   );
 }
-
-export default App;
+}
